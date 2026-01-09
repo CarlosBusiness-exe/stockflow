870988d655b0075f050dfafdc097fb8ba62d260b
CarlosBusiness-exe <business10carlos@gmail.com>
2026-01-09 09:38:34 -0300

    feat: Categories route was added on api_router

--- api/v1/api.py
+++ api/v1/api.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from api.v1.endpoints import product
+from api.v1.endpoints import category
 
 api_router = APIRouter()
 api_router.include_router(product.router, prefix="/products", tags=["products"])
+api_router.include_router(category.router, prefix="/categories", tags=["categories"])
