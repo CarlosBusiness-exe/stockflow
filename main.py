40944a716108427c5984f04d6a8b096245802678
CarlosBusiness-exe <business10carlos@gmail.com>
2026-01-09 08:36:44 -0300

    fix: the debug attribute was removed from uvicorn run

--- main.py
+++ main.py
@@ -9,4 +9,4 @@ app.include_router(api_router, prefix=settings.API_V1_STR)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, debug=True, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
