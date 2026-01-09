5c961d221621650f93bbbb83cbf78b87804a1e4d
CarlosBusiness-exe <business10carlos@gmail.com>
2026-01-09 13:52:18 -0300

    feat: supplier model was implemented

--- models/__all_models.py
+++ models/__all_models.py
@@ -1,2 +1,3 @@
 from models.product_model import ProductModel
 from models.category_model import CategoryModel
+from models.supplier_model import SupplierModel
---
+++ models/supplier_model.py
@@ -0,0 +1,11 @@
+from typing import Optional
+
+from sqlmodel import SQLModel, Field
+
+class SupplierModel(SQLModel, table=True):
+    __tablename__ = "suplliers"
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    name: str
+    cnpj: str = Field(unique=True)
+    address: str
