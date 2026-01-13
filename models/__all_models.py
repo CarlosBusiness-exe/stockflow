8dcfb3f7ec502875fa65dbff1c05a12f0aff9e97
CarlosBusiness-exe <business10carlos@gmail.com>
2026-01-13 19:22:55 -0300

    feat: UserModel has been added in __all_models

--- models/__all_models.py
+++ models/__all_models.py
@@ -1,3 +1,4 @@
 from models.product_model import ProductModel
 from models.category_model import CategoryModel
 from models.supplier_model import SupplierModel
+from models.user_model import UserModel
