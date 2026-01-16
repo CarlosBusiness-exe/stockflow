84e0d2d725364967ce9f340ee1a5fc629feffae9
CarlosBusiness-exe <business10carlos@gmail.com>
2026-01-16 09:40:36 -0300

    refactor: update supplier endpoints to use SQLModel schemas

--- api/v1/endpoints/supplier.py
+++ api/v1/endpoints/supplier.py
@@ -10,15 +10,18 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlmodel import select
 
 from models.supplier_model import SupplierModel
-from core.deps import get_session
+from models.user_model import UserModel
+from schemas.supplier_schema import SupplierSchemaBase, SupplierSchemaResponse
+from core.deps import get_session, get_current_user
 
 router = APIRouter()
 
 
 #POST SUPPLIER
-@router.post("/", response_model=SupplierModel, status_code=status.HTTP_201_CREATED)
-async def get_supplier(supplier: SupplierModel, db: AsyncSession = Depends(get_session)):
-    new_supplier = SupplierModel(name=supplier.name, cnpj=supplier.cnpj, address=supplier.address)
+@router.post("/", response_model=SupplierSchemaResponse, status_code=status.HTTP_201_CREATED)
+async def post_supplier(supplier: SupplierSchemaBase, db: AsyncSession = Depends(get_session), logged_user: UserModel = Depends(get_current_user)):
+    #new_supplier = SupplierModel(name=supplier.name, cnpj=supplier.cnpj, address=supplier.address)
+    new_supplier = SupplierModel.model_validate(supplier)
 
     db.add(new_supplier)
     await db.commit()
@@ -27,7 +30,7 @@ async def get_supplier(supplier: SupplierModel, db: AsyncSession = Depends(get_s
     return new_supplier
 
 #GET SUPPLIERS
-@router.get("/", response_model=List[SupplierModel], status_code=status.HTTP_200_OK)
+@router.get("/", response_model=List[SupplierSchemaResponse], status_code=status.HTTP_200_OK)
 async def get_suppliers(db: AsyncSession = Depends(get_session)):
     query = select(SupplierModel).order_by(SupplierModel.id)
     result = await db.execute(query)
@@ -37,7 +40,7 @@ async def get_suppliers(db: AsyncSession = Depends(get_session)):
 
 
 #GET SUPPLIER
-@router.get("/{supplier_id}", response_model=SupplierModel, status_code=status.HTTP_200_OK)
+@router.get("/{supplier_id}", response_model=SupplierSchemaResponse, status_code=status.HTTP_200_OK)
 async def get_supplier(supplier_id: int, db: AsyncSession = Depends(get_session)):
     query = select(SupplierModel).where(SupplierModel.id == supplier_id)
     result = await db.execute(query)
@@ -50,16 +53,15 @@ async def get_supplier(supplier_id: int, db: AsyncSession = Depends(get_session)
     
 
 #PUT SUPPLIER
-@router.put("/{supplier_id}", response_model=SupplierModel, status_code=status.HTTP_202_ACCEPTED)
-async def put_supplier(supplier_id: int, supplier: SupplierModel, db: AsyncSession = Depends(get_session)):
+@router.put("/{supplier_id}", response_model=SupplierSchemaResponse, status_code=status.HTTP_202_ACCEPTED)
+async def put_supplier(supplier_id: int, supplier: SupplierSchemaBase, db: AsyncSession = Depends(get_session), logged_user: UserModel = Depends(get_current_user)):
     query = select(SupplierModel).where(SupplierModel.id==supplier_id)
     result = await db.execute(query)
     supplier_up = result.scalar_one_or_none()
 
     if supplier_up:
-        supplier_up.name = supplier.name
-        supplier_up.cnpj = supplier.cnpj
-        supplier_up.address = supplier.address
+        supplier_data = supplier.model_dump(exclude_unset=True)
+        supplier_up.sqlmodel_update(supplier_data)
 
         await db.commit()
         await db.refresh(supplier_up)
@@ -71,7 +73,7 @@ async def put_supplier(supplier_id: int, supplier: SupplierModel, db: AsyncSessi
 
 #DELETE SUPPLIER
 @router.delete("/{supplier_id}", status_code=status.HTTP_204_NO_CONTENT)
-async def delete_supplier(supplier_id: int, db: AsyncSession = Depends(get_session)):
+async def delete_supplier(supplier_id: int, db: AsyncSession = Depends(get_session), logged_user: UserModel = Depends(get_current_user)):
     query = select(SupplierModel).where(SupplierModel.id == supplier_id)
     result = await db.execute(query)
     supplier_del = result.scalar_one_or_none()
